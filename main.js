28778f78e83ef5eef747c39802c9743a0b879623
Naman Kumar <naman.stakkdev@gmail.com>
2023-03-09 10:55:03 +0530

    'finalCommit'

--- main.js
+++ main.js
@@ -1,103 +1,96 @@
-const { app, BrowserWindow, Menu, webContents, ipcMain, Tray} = require('electron')
-const log = require('electron-log')
-const Store = require('./Store')
-
+const { app, Menu, ipcMain } = require("electron");
+// const log = require("electron-log");
+const Store = require("./Store");
+const path = require("path");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
 // Set env
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = "production";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true : false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow
-let tray
+let mainWindow;
+let tray;
 
-const store= new Store({
-  configName:'user-settings',
-  defaults:{
-    settings:{
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
       cpuoverload: 1,
       alertFrequency: 1,
-    }
-  }
-})
+    },
+  },
+});
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'SysTop',
-    width: isDev ? 700 : 355,
-    height: 600,
-    icon: './assets/icons/icon.png',
-    resizable: isDev ? true : false,
-    show: false,
-    opacity: 0.9,
-    backgroundColor: 'white',
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  })
-
-  if (isDev) {
-    mainWindow.webContents.openDevTools()
-  }
-
-  mainWindow.loadFile('./app/index.html')
+  mainWindow = new MainWindow("./app/index.html", isDev);
 }
 
-app.on('ready', () => {
-  createMainWindow()
-  mainWindow.webContents.on('dom-ready',()=>{
-    mainWindow.webContents.send('settings.get',store.get('settings'))
-  })
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
+app.on("ready", () => {
+  createMainWindow();
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings.get", store.get("settings"));
+  });
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-  const icon=path.join(__dirname,'assets','icons','tray_icon.png')
-  tray =new Tray(icon)
-  tray.on('click',()=>
-  {
-    if(mainWindow.isVisible()===true)
-      mainWindow.hide()
-    else{
-      mainWindow.show()
+  mainWindow.on("close", (e) => {
+    if (!app.isQuiting) {
+      e.preventDefault();
+      mainWindow.hide();
     }
-    })
-})
+    return true;
+  });
+
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
+  tray = new AppTray(icon, mainWindow);
+});
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+  ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    role: 'fileMenu',
+    role: "fileMenu",
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle"),
+      },
+    ],
   },
   ...(isDev
     ? [
         {
-          label: 'Developer',
+          label: "Developer",
           submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
-            { type: 'separator' },
-            { role: 'toggledevtools' },
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
           ],
         },
       ]
     : []),
-]
+];
 
-ipcMain.on('settings:set',(e,value)=>{
-    store.set('settings',value)
-    mainWindow.webContents.send('settings.get',store.get('settings'))
-})
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings.get", store.get("settings"));
+});
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
+    createMainWindow();
   }
-})
+});
 
-app.allowRendererProcessReuse = true
+app.allowRendererProcessReuse = true;
